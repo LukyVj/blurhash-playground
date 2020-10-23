f50fe310ad4cfba7e694083c701720f8f08a0610
LukyVj <lucas.bonomi@gmail.com>
2020-10-23 14:53:44 +0200

    update favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,12 @@ const Home = () => {
     <div className="container">
       <Head>
         <title>BlurHash Playground - @LukyVj</title>
-        <link rel="icon" href="/favicon.svg" />
+        {/* <link rel="icon" href="/favicon.svg" /> */}
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%220%22 fill=%22%230000ff%22></rect><path stroke-width=%223%22 stroke=%22white%22 d=%22M28.77 80L9.04 80L9.04 23.13L27.64 23.13Q36.89 23.13 41.56 26.95Q46.23 30.78 46.23 38.28L46.23 38.28Q46.23 42.27 43.96 45.33Q41.70 48.40 37.79 50.08L37.79 50.08Q42.40 51.37 45.08 54.98Q47.75 58.59 47.75 63.59L47.75 63.59Q47.75 71.25 42.79 75.63Q37.83 80 28.77 80L28.77 80ZM29.04 53.40L16.54 53.40L16.54 73.87L28.93 73.87Q34.16 73.87 37.19 71.15Q40.21 68.44 40.21 63.67L40.21 63.67Q40.21 53.40 29.04 53.40L29.04 53.40ZM16.54 29.30L16.54 47.38L27.87 47.38Q32.79 47.38 35.74 44.92Q38.69 42.46 38.69 38.24L38.69 38.24Q38.69 33.55 35.96 31.43Q33.22 29.30 27.64 29.30L27.64 29.30L16.54 29.30ZM64.94 20L64.94 42.85Q69.75 36.95 77.44 36.95L77.44 36.95Q90.84 36.95 90.96 52.07L90.96 52.07L90.96 80L83.73 80L83.73 52.03Q83.69 47.46 81.64 45.27Q79.59 43.09 75.25 43.09L75.25 43.09Q71.74 43.09 69.08 44.96Q66.43 46.84 64.94 49.88L64.94 49.88L64.94 80L57.71 80L57.71 20L64.94 20Z%22 fill=%22%23fff%22></path></svg>"
+        />
 
         <script
           async
@@ -67,7 +72,7 @@ const Home = () => {
 
             <div className="md:pos-absolute top-0 md:right-0 md:m-32 d-flex ai-center ta-center fxd-column">
               <button
-                className="w-100p cursor-pointer"
+                className="w-100p cursor-pointer m-0"
                 onClick={() => setReload(!reload)}
               >
                 I don't like this background
@@ -122,7 +127,7 @@ const Home = () => {
             {"  "}
             by{"  "}
             <a href="https://twitter.com/lukyvj">@Lukyvj</a> - Hosted on{" "}
-            <a href="https://vercel.app/">https://vercel.app/</a>
+            <a href="https://vercel.app/">▲ https://vercel.app/</a>
           </footer>
         </div>
       </main>
